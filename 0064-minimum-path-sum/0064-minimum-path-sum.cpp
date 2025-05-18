ef2aa560bea6bd72daab8a07ffdc2a567d633644
Yogesh Bora <borashubham02@gmail.com>
2025-05-18 22:12:30 +0530

    Time: 0 ms (100%), Space: 15.4 MB (86.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int dp[201][201];
     int func(int r , int c , int n , int m , vector<vector<int>>& obs){
-        if(r >= n || c >= m) return 1e8;
+        if(r >= n || c >= m) return INT_MAX;
 
         if(r == n-1 && c == m-1) return obs[r][c];
 
         if(dp[r][c] != -1) return dp[r][c];
 
-        int down = obs[r][c] + func(r+1 , c , n , m , obs);
 
-        int right = obs[r][c] + func(r , c+1, n , m , obs);
+        int right = obs[r][c] + min(func(r+1 , c , n , m , obs) , func(r , c+1, n , m , obs));
 
-        return dp[r][c] = min(down , right);
+        return dp[r][c] = right;
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size();
