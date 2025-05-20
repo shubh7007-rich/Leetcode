ced5f4a68a0cfb2bc9c87bddf3d2e51136920d95
Yogesh Bora <borashubham02@gmail.com>
2025-05-20 20:42:22 +0530

    Time: 1932 ms (5.01%), Space: 546.9 MB (5.11%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isPos(int mid , vector<int>& nums, vector<vector<int>>& q , int n){
+        vector<int> diff(n+1 , 0);
+
+        for(int i = 0 ; i < mid ; i++){
+            vector<int> vec = q[i];
+
+            diff[vec[0]] += vec[2];
+            diff[vec[1] + 1] -= vec[2];
+        }
+
+        for(int i = 1 ; i < n+1 ; i++){
+            diff[i] += diff[i-1];
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] > diff[i]) return false;
+        }
+
+        return true;
+    }
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& q) {
+        int l = 0 , r = q.size() , ans = -1 , n = nums.size();
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+
+            if(isPos(mid , nums , q , n)){
+                ans = mid;
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
