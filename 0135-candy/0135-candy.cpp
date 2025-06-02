cbf43f81eea1a1da00e40d59f51a6534bcab9e76
Yogesh Bora <borashubham02@gmail.com>
2025-06-02 11:45:33 +0530

    Time: 0 ms (100%), Space: 23 MB (43.5%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    /* 
+        So here for every index i need satisfy the conditions
+        1. Each child must have at least one candy.
+        2. Children with a higher rating get more candies than their neighbors.
+        So for every index (expect the first and the last) we need to check it's left and  right neighbour to satisfying the condition
+        So for ques like these first go from L 2 R satisfying the condition left neighbour ko dekhte huwe and then go from R 2 L satisfying the condition right neighbour ko dekhte huwe
+    */
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> ans(n , 1);
