da6de4493c7cde22c5b97c000a2599e83561e5d8
Yogesh Bora <borashubham02@gmail.com>
2025-06-02 11:33:51 +0530

    Time: 0 ms (100%), Space: 22.9 MB (59.49%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> ans(n , 1);
+
+        if(n == 1) return 1;
+
+        for(int i = 1 ; i < n ; i++){
+            if(ratings[i] > ratings[i-1]){
+                ans[i] = ans[i-1] + 1;
+            }
+        }
+
+        for(int i = n-2 ; i >= 0 ; i--){
+            if(ratings[i] > ratings[i+1] && ans[i] <= ans[i+1]){
+                ans[i] = ans[i+1] + 1;
+            }
+        }
+        int sum = 0;
+
+        for(int & i : ans) sum += i;
+
+        return sum;
+    }
+};
