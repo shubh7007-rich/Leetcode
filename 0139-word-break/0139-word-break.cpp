5c286f5486fed65fc7cf1317cda94298e7578d51
Yogesh Bora <borashubham02@gmail.com>
2025-06-03 13:47:56 +0530

    Time: 11 ms (48.53%), Space: 16.5 MB (34.44%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+   
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> st(wordDict.begin() , wordDict.end());
+
+        queue<int> que;
+
+        int n = s.length();
+
+        vector<int> vis(n , 0);
+
+        que.push(0);
+
+        while(!que.empty()){
+            int start = que.front();
+            que.pop();
+
+            if(start == s.length()) return true;
+
+            for(int end = start ; end < s.length() ; end++){
+
+                if(vis[end]) continue;
+                
+                if(st.find(s.substr(start , end - start + 1)) != st.end()){
+                    que.push(end+1);
+                    vis[end] = 1;
+                }
+            }
+
+        }
+
+
+        return false;
+    }
+};
