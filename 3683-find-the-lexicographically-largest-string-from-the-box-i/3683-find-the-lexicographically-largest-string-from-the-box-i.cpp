7d6b82d82f46330a84716daec041ecbb7784eed1
Yogesh Bora <borashubham02@gmail.com>
2025-06-04 14:18:15 +0530

    Time: 79 ms (23.88%), Space: 169.3 MB (24.63%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -3,19 +3,12 @@ public:
     string answerString(string word, int numFriends) {
         int n = word.length();
 
-        vector<string> vec;
-
         if(numFriends == 1) return word;
-
-        int len = n - numFriends + 1;
-
-        if(n == numFriends) len = 1;
-
-        for(int i = 0 ; i <= word.length(); i++){
-            vec.push_back(word.substr(i , len));
+        string ans = "";
+        for(int i = 0 ; i < n ; i++){
+            ans = max(ans , word.substr(i , n - numFriends + 1));
         }
-        sort(vec.rbegin() , vec.rend());
 
-        return vec[0];
+        return ans;
     }
 };
