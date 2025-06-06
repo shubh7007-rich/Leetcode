5316529f7bc5ecd819d14405e5095cc8ad39402e
Yogesh Bora <borashubham02@gmail.com>
2025-06-06 14:35:09 +0530

    Time: 136 ms (17.82%), Space: 35 MB (30.2%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     string robotWithString(string s) {
-        map<char, int> mp;
-        for (char ch : s) mp[ch]++;
+        map<char , int> mp;
 
-        string t = "", ans = "";
+        for(char ch : s) mp[ch]++;
 
-        for (int i = 0; i < s.length(); ++i) {
-            char curr = s[i];
-            t.push_back(curr);
-            mp[curr]--;
-            if (mp[curr] == 0) mp.erase(curr);
+        string t = "" , ans = "";
 
-            // Pop as many chars from stack t if they are <= smallest remaining
-            while (!t.empty() && (mp.empty() || t.back() <= mp.begin()->first)) {
+        for(int i = 0; i < s.length() ; i++){
+            while((!t.empty() && (t.back() <= mp.begin()->first))){
                 ans += t.back();
                 t.pop_back();
             }
+            t.push_back(s[i]);
+            mp[s[i]]--;
+            if(mp[s[i]] == 0){
+                mp.erase(s[i]);
+            }
         }
+
+        while(!t.empty()){
+            ans.push_back(t.back());
+            t.pop_back();
+        }  
 
         return ans;
     }
