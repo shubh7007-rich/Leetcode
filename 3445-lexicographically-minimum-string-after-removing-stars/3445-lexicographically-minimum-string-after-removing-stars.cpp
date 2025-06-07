6cf4d2d652df961686864e1541b1e1fd26207727
Yogesh Bora <borashubham02@gmail.com>
2025-06-07 08:49:33 +0530

    Time: 133 ms (57.79%), Space: 37.2 MB (33.44%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,43 +1,52 @@
 class Solution {
 public:
     string clearStars(string s) {
-        using pii = pair<char ,int>;
 
-        auto cmp = [](pii a , pii b){
+        /* 
+            cmp -
+                for example if cmp has a return statement like (return a < b) , so if it returns true
+                that means a has less priority than b it will remain below b in the heap 
+                so a < b will be used to define a max-heap
+                and a > b will be used to define a min-heap
+        */
+
+        using pii = pair<int ,int>;
+        auto cmp = [](pii & a , pii & b){
             if(a.first == b.first){
-                return a.second < b.second;
+                return a.second < b.second;    
             }
 
             return a.first > b.first;
         };
 
+        int n = s.length();    
+
         priority_queue<pii , vector<pii> , decltype(cmp)> pq;
 
-        vector<int> marked(s.length() , 0);
+        vector<int> marked(n , 0);
 
-        for(int i = 0 ; i < s.length() ; i++){
+        for(int i = 0 ; i < n ; i++){
             if(s[i] == '*'){
                 marked[i] = 1;
 
                 if(!pq.empty()){
                     auto p = pq.top();
-                    marked[p.second] = 1;
                     pq.pop();
 
+                    marked[p.second] = 1;
                 }
             }else{
                 pq.push({s[i] , i});
             }
         }
+        string str = "";
 
-        string ans = "";
-
-        for(int i = 0 ; i < s.length() ; i++){
-            if(marked[i] == 0){
-                ans += s[i];
+        for(int i = 0 ;i  < n ; i++){
+            if(!marked[i]){
+                str += s[i];
             }
         }
 
-        return ans;
+        return str;
     }
-};
+}; 
