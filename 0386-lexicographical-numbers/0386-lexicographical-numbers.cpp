e94ac2d253c444806b8fc01d9f3523c504fe98db
Yogesh Bora <borashubham02@gmail.com>
2025-06-09 17:44:22 +0530

    Time: 6 ms (29.35%), Space: 14.2 MB (54.02%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    void dfs(int num , vector<int>& ans , int n){
-        if(num > n) return;
-        
+    void func(int num , int n , vector<int>& ans){
         ans.push_back(num);
 
         for(int i = 0 ; i <= 9 ; i++){
             int temp = num;
             temp = temp * 10 + i;
-
-            if(temp <= n) dfs(temp , ans , n);
+            if(temp <= n) func(temp , n , ans);
         }
     }
     vector<int> lexicalOrder(int n) {
-        int num = 1;
         vector<int> ans;
 
         for(int i = 1 ; i <= 9 ; i++){
-            dfs(i , ans , n);
+            if(i <= n) func(i , n , ans);
         }
 
         return ans;
