d741688cff35619e605c4e339c09b7c8b233f71b
Yogesh Bora <borashubham02@gmail.com>
2025-07-04 15:08:22 +0530

    Time: 59 ms (5.74%), Space: 51.6 MB (90.77%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,24 +1,22 @@
 #define ll long long
-
 class Solution {
 public:
     int mod = 1e9 + 7;
 
-    ll func(ll a, ll b) {
-        ll res = 1;
-
+    int power(int a , int b){
+        ll result = 1;
         while(b){
             if(b&1){
-                res = (a * res) % mod;
+                result = (result * a) % mod;
             }
-            a = (a * a)%mod;
+            a = (a * 1ll * a) % mod;
             b >>= 1;
         }
 
-        return res;
+        return result % mod;
     }
     int numSubseq(vector<int>& nums, int target) {
-        ll ans = 0;
+        int cnt = 0;
 
         sort(nums.begin() , nums.end());
 
@@ -27,9 +25,9 @@ public:
 
             int idx = upper_bound(nums.begin() , nums.end() , tar) - nums.begin() - 1;
 
-            if(idx >= i)
-                ans = (ans + (func(2 , idx - i)))%mod;
+            if( idx >= i ) cnt = (cnt +  power(2 , idx - i))%mod;
         }
-        return ans;
+
+        return cnt;
     }
 };
