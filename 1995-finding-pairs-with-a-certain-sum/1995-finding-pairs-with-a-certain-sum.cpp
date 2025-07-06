e980ca09bf70a7e58031a9c673b3945743a3ba35
Yogesh Bora <borashubham02@gmail.com>
2025-07-06 12:53:12 +0530

    Time: 244 ms (43.01%), Space: 85.1 MB (40.5%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
@@ -1,20 +1,18 @@
 class FindSumPairs {
 public:
     vector<int> n1 , n2;
-    map<int , int> mp1 , mp2;
+    map<int , int> mp2;
     
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
         // i can sort nums1
         // so for every index in num2 i can find 
         for(int i : nums1){
             n1.push_back(i);
-            mp1[i]++;
         }
         for(int i : nums2){
             n2.push_back(i);
             mp2[i]++;
         }
-        sort(n1.begin() , n1.end());
     }
     
     void add(int index, int val) {
@@ -22,17 +20,19 @@ public:
         n2[index] += val;
         mp2[n2[index]]++;
     }
-    
+
+    // just traverse in nums1
+
     int count(int tot) {
         int cnt = 0;
         
-        for(auto [num , freq] : mp1){
-            int tar = tot - num;
+        for(int i = 0 ; i < n1.size() ; i++){
+            int tar = tot - n1[i];
 
             if(tar < 0) continue;
 
             if(mp2.find(tar) != mp2.end()){
-                cnt += (freq * mp2[tar]);
+                cnt += mp2[tar];
             }
         }
 
