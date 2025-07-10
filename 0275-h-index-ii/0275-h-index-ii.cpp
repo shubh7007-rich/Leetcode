968afaf768fb4050152ad2e7880b2b36b0d91789
Yogesh Bora <borashubham02@gmail.com>
2025-07-10 22:22:16 +0530

    Time: 4 ms (4.38%), Space: 22.6 MB (12.63%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isPos(int mid , vector<int>& cit){
+        int c = 0;
+
+        for(int i = 0 ; i < cit.size() ; i++){
+            if(cit[i] >= mid) c++;
+        }
+
+        return c >= mid;
+    }
+    int hIndex(vector<int>& cit) {
+        sort(cit.begin() , cit.end());
+
+        int n = cit.size();
+
+        int l = 0 , r = n , ans = 0;
+
+        while(l <= r){
+            int mid = (l+r)/2;
+
+            // int issJada = n - mid;
+
+            if(isPos(mid , cit)){
+                ans = mid; 
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
