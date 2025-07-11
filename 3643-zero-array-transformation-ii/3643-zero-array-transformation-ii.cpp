28bca63cd43945cf7fa91440402c802da9fee53d
Yogesh Bora <borashubham02@gmail.com>
2025-07-11 09:40:52 +0530

    Time: 77 ms (37.43%), Space: 347.6 MB (28.25%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    bool isPos(int mid , vector<int>& nums, vector<vector<int>>& q , int n){
+    bool isPos(int mid , vector<int>& nums, vector<vector<int>>& queries){
+        int ind = mid - 1 , n = nums.size();
+
         vector<int> diff(n+1 , 0);
 
-        for(int i = 0 ; i < mid ; i++){
+        for(int i = 0 ; i <= ind  ; i++){
+            int idx1 = queries[i][0] , idx2 = queries[i][1] , val = queries[i][2];
 
-            diff[q[i][0]] += q[i][2];
-            diff[q[i][1] + 1] -= q[i][2];
+            diff[idx1] += val;
+            diff[idx2 + 1] -= val; 
         }
 
-        for(int i = 1 ; i < n+1 ; i++){
-            diff[i] += diff[i-1];
-        }
+        for(int i = 1 ; i <= n ; i++) diff[i] += diff[i-1];
 
         for(int i = 0 ; i < n ; i++){
             if(nums[i] > diff[i]) return false;
@@ -19,13 +20,14 @@ public:
 
         return true;
     }
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& q) {
-        int l = 0 , r = q.size() , ans = -1 , n = nums.size();
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int l = 0 , r = queries.size()  , ans = -1;
 
         while(l <= r){
             int mid = l + (r-l)/2;
 
-            if(isPos(mid , nums , q , n)){
+            if(isPos(mid , nums , queries)){
                 ans = mid;
                 r = mid - 1;
             }else{
