6aec4faac94e1c9cf1051d9e8d449ca39bedebba
Yogesh Bora <borashubham02@gmail.com>
2025-07-13 09:38:13 +0530

    Time: 0 ms (100%), Space: 32.2 MB (27.19%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -3,15 +3,26 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int x = *min_element(nums.begin(), nums.end());
-        priority_queue<int> pq(nums.begin(), nums.end());
-        int moves = 0;
-        while (pq.top() > x) {
-            int t1 = pq.top();
-            pq.pop();
-            moves += abs(t1-x);
-        }  
+        // [1 , 2, 3]     ----->   [4 , 4, 4]
 
-        return  moves;
+        // suppose we need m moves to make all elements equal
+
+        // 1st observation -->  1 + 3 = 4
+
+        //                    1st eq --> mini + m = x  
+
+        // 2nd observation -->      n * x = intialSum + m * (n-1) 
+
+        //                         n * (mini + m) = intialSum + m * n - m
+        //                         n * mini + n*m = intialSum + m*n - m
+        //                         m = intialSum - n * mini
+
+        int intialSum = accumulate(nums.begin() , nums.end() , 0);
+
+        int mini = *min_element(nums.begin() , nums.end());
+
+        return intialSum - nums.size() * mini;
+
+
     }
 };
