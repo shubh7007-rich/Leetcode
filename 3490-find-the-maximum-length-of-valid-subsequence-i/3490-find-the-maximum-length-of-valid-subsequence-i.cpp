9a4e6da7042a876a6b5a8ec710ea4bca71c7b147
Yogesh Bora <borashubham02@gmail.com>
2025-07-16 11:29:25 +0530

    Time: 12 ms (29.41%), Space: 96.2 MB (62.35%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        // % 2 so we can get either 0 or 1 
+        // 0 ---> our sum every two elements needs to be even , for our sum needs to even both ele should be odd or even
+        // 1 --> our sum needs to be odd , for our sum needs to be odd we need both the element of different parity
+
+        int l1 = 0 , l2 = 0 , l3 = 0 , l4 = 0;
+
+        // all odds
+
+        for(int & i : nums){
+            if(i&1) l1++;
+        }
+
+        // all even
+        for(int & i : nums){
+            if(!(i&1)) l2++;
+        }
+
+        // even odd
+        bool flag = true;
+
+        for(int & i : nums){
+            if(flag){  // even
+                if(!(i&1)){
+                    l3++;
+                    flag = !flag;
+                }
+                
+            }else{  // odd
+                if(i&1){
+                    l3++;
+                    flag = !flag;
+                }
+                
+            }
+        }
+
+        // odd even
+        flag = true;
+
+        for(int & i : nums){
+            if(flag){  // odd
+                if(i&1){
+                    l4++;
+                    flag = !flag;
+                }
+                
+            }else{  // even
+                if(!(i&1)){
+                    flag = !flag;
+                    l4++;
+                }
+                
+            }
+        }
+
+
+        return max({l1 , l2 , l3 , l4});
+
+    }
+};
