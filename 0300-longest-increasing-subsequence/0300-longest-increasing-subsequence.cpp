56c61f094b9c570c21b0de3b56206936df4317a5
Yogesh Bora <borashubham02@gmail.com>
2025-07-19 08:44:49 +0530

    Time: 299 ms (31.38%), Space: 41.5 MB (40.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    int dp[2501][2500];
+    int func(int prev , int ind , vector<int>& nums , int n){
+        if(ind >= n) return 0;
+
+        int take = 0 , skip = 0;
+
+        if(dp[prev + 1][ind] != -1) return dp[prev + 1][ind];
+
+        if(prev == -1 || (nums[prev] < nums[ind])){
+            take = 1 + func(ind , ind + 1 , nums ,  n);
+        }
+
+        skip = func(prev , ind + 1 , nums ,  n);
+
+        return dp[prev + 1][ind] = max(skip , take);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        memset(dp , -1 , sizeof(dp));
+        // int  maxi = 0;
 
-        vector<int> lis(n , INT_MAX);
+        int maxi = func(-1 , 0 , nums , nums.size());
+        
 
-        for(int & i : nums){
-            int idx = lower_bound(lis.begin() , lis.end() , i) - lis.begin();
+        return maxi;
 
-            lis[idx] = i;
-        }
-
-        int cnt = 0;
-
-        for(int & i : lis){
-            if(i == INT_MAX) break;
-            cnt++;
-        }
-
-        return cnt;
     }
 };
