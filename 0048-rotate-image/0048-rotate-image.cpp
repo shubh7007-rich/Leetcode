6064032f95f333d5a3e2588dfdfa2aac2cb13926
Yogesh Bora <borashubham02@gmail.com>
2025-07-24 17:03:50 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,16 +3,23 @@ public:
     void rotate(vector<vector<int>>& mat) {
         int n = mat.size() , m = mat[0].size();
 
-        vector<vector<int>> copy(n , vector<int>(m , 0));
-        int p = 0 , q = 0;
+        /*
+    -> rotating by 90* is just taking every row and placing it vertically from the last -> first column
+    -> so if we look closely , if we want to rotate our matrix by 90 degree then we just tranpose our matrix and reverse every row.
+    -> this will not require an extra space
+         */
 
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                copy[j][n-1-i] = mat[i][j];
+         // tranposing our matrix
+
+         for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < m ; j++){
+                swap(mat[i][j] , mat[j][i]);
             }
-        }
+         }
 
-        mat = copy;
+         for(vector<int>& vec : mat){
+            reverse(vec.begin() , vec.end());
+         }
+
     }
 };
