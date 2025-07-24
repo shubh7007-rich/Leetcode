f37b248b12453a7cd69d4ce126ba3e60f53ee5bd
Yogesh Bora <borashubham02@gmail.com>
2025-07-24 16:47:26 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -6,12 +6,11 @@ public:
         vector<vector<int>> copy(n , vector<int>(m , 0));
         int p = 0 , q = 0;
 
-        for(int j = m-1 ; j >= 0 ; j--){
-            for(int i = 0 ; i < n ; i++){
-                copy[i][j] = mat[p][q];
-                q++;
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                copy[j][n-1-i] = mat[i][j];
             }
-            p++;q=0;
         }
 
         mat = copy;
