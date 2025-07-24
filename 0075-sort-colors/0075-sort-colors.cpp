137fdf8bafcd2fb4e9490003bf5320785e2d701e
Yogesh Bora <borashubham02@gmail.com>
2025-07-24 07:50:50 +0530

    Time: 3 ms (3.02%), Space: 15.4 MB (12.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-            //logic behind it is that all zeroes should be at low index , one at mid index and two at high index 
-            // int low , mid , high ;
-            // low = mid = 0 , high = nums.size() - 1 ;
-            // for(int i = 0 ; i < nums.size() ; i++){
-            //     if(nums[mid] == 0){
-            //         swap(nums[low] , nums[mid]);
-            //         low++ , mid++;
-            //     }
-            //     else if(nums[mid] == 1){
-            //         mid++;
-            //     }
-            //     else{
-            //         swap(nums[mid] , nums[high]);
-            //         high-- ;
-            //     }
-            // }
+    void merge(int lb , int mid , int ub , vector<int>& nums){
+        vector<int> temp;
+        int p1 = lb , p2 = mid + 1;
 
-            for(int i = 0 ; i < nums.size() - 1 ;i++){
-                int minIndex = i ;
-                for(int j = i+1 ; j < nums.size()  ; j++){
-                    if(nums[j] < nums[minIndex]){
-                        minIndex = j;
-                    }
-                }
-                if(nums[i] > nums[minIndex]){
-                    swap(nums[i] , nums[minIndex]);
-                }
+        while(p1 <= mid && p2 <= ub){
+            if(nums[p1] <= nums[p2]){
+                temp.push_back(nums[p1]);
+                p1++;
+            }else{
+                temp.push_back(nums[p2]);
+                p2++;
             }
+        }
+
+        while(p1 <= mid){
+            temp.push_back(nums[p1]);
+            p1++;
+        }
+        while(p2 <= ub){
+             temp.push_back(nums[p2]);
+             p2++;
+        }
+        int p3 = 0;
+
+        for(int i = lb ; i <= ub ; i++){
+            nums[i] = temp[i - lb];
+        }
+    }
+    void mergeS(int lb , int ub , vector<int>& nums){
+        if(lb < ub){
+            int mid = (lb + ub)/2;
+
+            mergeS(lb , mid , nums);
+            mergeS(mid+1 , ub , nums);
+            merge(lb , mid , ub , nums);
+        }
+    }
+    void sortColors(vector<int>& nums) {
+        int lb = 0 , ub = nums.size() - 1;
+        mergeS(lb , ub , nums);
     }
 };
