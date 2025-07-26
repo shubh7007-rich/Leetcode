69b9483579d2e12ee4d62d2f32035d8f65066c89
Yogesh Bora <borashubham02@gmail.com>
2025-07-26 09:42:13 +0530

    Time: 3 ms (70.53%), Space: 15 MB (20.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,12 +3,28 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // t.c - O(n*n)
 
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     for(int j = i+1 ; j < nums.size() ; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return {i , j};
+        //         }
+        //     }
+        // }
+
+        // return {-1 , -1};
+
+        // Optimal approach --> t.c O(n)
+
+        unordered_map<int , int> mp;
+
         for(int i = 0 ; i < nums.size() ; i++){
-            for(int j = i+1 ; j < nums.size() ; j++){
-                if(nums[i] + nums[j] == target){
-                    return {i , j};
-                }
+            int check = target - nums[i];
+
+            if(mp.find(check) != mp.end()){
+                return {i , mp[check]};
             }
+
+            mp[nums[i]] = i;
         }
 
         return {-1 , -1};
