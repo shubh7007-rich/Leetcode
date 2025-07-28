c79ff6fc3e07a764d0f5e07a9b3cd01970ede914
Yogesh Bora <borashubham02@gmail.com>
2025-07-28 09:29:41 +0530

    Time: 77 ms (25.97%), Space: 10.6 MB (86.47%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -7,8 +7,7 @@ public:
 
         for(int & i : nums) maxOr |= i;
 
-        for(int i = 0 ; i <= (1 << n) ; i++){
-            
+        for(int i = 0 ; i < (1 << n) ; i++){
             int orr = 0;
             for(int j = 0 ; j < nums.size() ; j++){
                 if(i & (1 << j)){
