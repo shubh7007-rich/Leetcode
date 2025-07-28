751ca2be0e43deef85ee55596ccd223550ed7aaf
Yogesh Bora <borashubham02@gmail.com>
2025-07-28 17:55:23 +0530

    Time: 1 ms (80.91%), Space: 10.2 MB (75.5%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,63 +1,58 @@
 class Solution {
 public:
-    bool isSafe(int r , int c , vector<string>& board , int n){
-        
-        int r1 = r , c1 = c;
-
-        while(r1 >= 0){
-            if(board[r1][c1] == 'Q') return false;
-            r1--;
-        }
-        r1 = r;
+    bool isSafe(int row , int col , vector<string>& board , int n){
+        int r1 = row - 1;
+        int c1 = col - 1;
 
         while(r1 >= 0 && c1 >= 0){
             if(board[r1][c1] == 'Q') return false;
             r1--;
             c1--;
         }
-
-        r1 = r;
-        c1 = c;
-
-         while(r1 >= 0 && c1 < n){
+        c1 = col - 1 , r1 = row+1;
+        while(r1 < n && c1 >= 0){
             if(board[r1][c1] == 'Q') return false;
-            r1--;
-            c1++;
+            r1++;
+            c1--;
         }
+
+        r1 = row , c1 = col-1;
+        while(c1 >= 0){
+            if(board[r1][c1] == 'Q') return false;
+            c1--;
+        }
+
         return true;
     }
-    void func(int ind , vector<string>& board , vector<vector<string>> & ans , int n){
-
-        if(ind == n){
+    void func(int col , vector<string>& board , int n , vector<vector<string>>& ans){
+        if(col >= n){
             ans.push_back(board);
             return;
         }
 
-        for(int i = 0 ; i < n ; i++){
-            if(isSafe(ind , i , board , n)){
-                board[ind][i] = 'Q';
-                func(ind+1 , board , ans , n);
-                board[ind][i] = '.';
+        for(int row = 0 ; row < n ; row++){
+            if(isSafe(row , col , board , n)){
+                board[row][col] = 'Q';
+                func(col+1 , board , n , ans);
+                board[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-
         vector<string> board;
-
         string str(n , '.');
+
+        vector<vector<string>> ans;
 
         for(int i = 0 ; i < n ; i++){
             board.push_back(str);
         }
 
-        for(int i = 0 ; i < n ; i++){
-            board[0][i] = 'Q';
-            func(1 , board , ans , n);
-            board[0][i] = '.';
+        for(int row = 0 ; row < n ; row++){
+            board[row][0] = 'Q';
+            func(1 , board , n , ans);
+            board[row][0] = '.';
         }
-
 
         return ans;
     }
