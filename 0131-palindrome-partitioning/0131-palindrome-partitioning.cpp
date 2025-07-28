ccea24b1f7ef4c949dec2540abef30cf21dcfaf5
Yogesh Bora <borashubham02@gmail.com>
2025-07-28 08:24:36 +0530

    Time: 29 ms (92.4%), Space: 53.5 MB (85.36%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(int start , int end , string & s){
+
+        while(start < end){
+            if(s[start] != s[end]) return false;
+            start++;end--;
+        }
+
+        return true;
+    }
+    void func(int ind , vector<string>& temp , vector<vector<string>>& ans , string & s , int n){
+        if(ind >= n){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = ind ; i < n ; i++){
+            if(isPalindrome(ind , i , s)){
+                temp.push_back(s.substr(ind , i-ind+1));
+                func(i+1 , temp , ans , s , n);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        int n = s.length();
+
+        vector<string> temp;
+        vector<vector<string>> ans;
+
+        for(int i = 0 ; i < n ; i++){
+            if(isPalindrome(0 , i , s)){
+                temp.push_back(s.substr(0 , i - 0 + 1));
+                func(i+1 , temp , ans , s , n);
+                temp.pop_back();
+            }
+        }
+
+        return ans;
+    }
+};
