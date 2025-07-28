804a47cdd6610f36e2b3262082857798a0315f31
Yogesh Bora <borashubham02@gmail.com>
2025-07-28 09:28:51 +0530

    Time: 1332 ms (5.07%), Space: 375.8 MB (7%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int cnt = 0 , maxi = 0;
+    int countMaxOrSubsets(vector<int>& nums) {
+        // appraoch 1 - generate all subsets and check maximum
 
-    void func(int ind , int orr , vector<int>& nums){
-        if(ind >= nums.size()){
-            if(orr > maxi){
-                cnt = 1;
-                maxi = orr;
-            }else{
-                if(orr == maxi){
-                    cnt++;
+        int n = nums.size() ,maxOr = 0 , cnt = 0;
+
+        for(int & i : nums) maxOr |= i;
+
+        for(int i = 0 ; i <= (1 << n) ; i++){
+            vector<int> subset;
+            int orr = 0;
+            for(int j = 0 ; j < nums.size() ; j++){
+                if(i & (1 << j)){
+                    subset.push_back(nums[j]);
+                    orr |= nums[j];
                 }
             }
 
-            return;
+            if(orr == maxOr) cnt++;
         }
-        int temp = orr;
-        orr = orr | nums[ind];
-        func(ind + 1 , orr , nums);
-        func(ind + 1 , temp , nums);
-    }
 
-    int countMaxOrSubsets(vector<int>& nums) {
-        int orr = 0;
-        func(0 ,orr , nums);
         return cnt;
+
+
+        // approach 2 - recursion , T.C - 2^n
     }
 };
