f281964f1186a45f322fcfc4a4342feebdd62363
Yogesh Bora <borashubham02@gmail.com>
2025-07-30 09:06:10 +0530

    Time: 13 ms (44.73%), Space: 18.7 MB (11.41%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,6 +9,9 @@
  */
 class Solution {
 public:
+    /*
+
+    Approach 2 -> T.C - O(n) , S.C - O(1)
     TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
         if(!root) return NULL;
 
@@ -23,7 +26,48 @@ public:
 
         return right;
     }
+    */
+    bool getPath(TreeNode* root, TreeNode* target , vector<TreeNode*> &vec){
+        if(root == NULL) return false;
+
+        vec.push_back(root);
+        
+        if(root == target) return true;
+
+        if(getPath(root->left , target , vec)){
+            return true;
+        }
+
+        if(getPath(root->right , target , vec)){
+            return true;
+        }
+
+        vec.pop_back();
+
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return solve(root , p , q);
+        // return solve(root , p , q);
+
+        //     Appraoch 1 -> We can store paths to both of the nodes and store
+        //     them in a vector and then traverse both of the vectors
+        //     simultaneously and return the first common node
+        // T.C - O(N) , S.C - O(n)
+
+        vector<TreeNode*> pathQ, pathP;
+        TreeNode *ans = NULL;
+
+        if(root == p || root == q) return root;
+
+        getPath(root, p, pathP);
+        getPath(root, q, pathQ);
+
+        int len = min(pathP.size() , pathQ.size());
+
+        for(int i = 0 ; i < len ; i++){
+            if(pathQ[i] == pathP[i]) ans = pathQ[i];
+        }
+
+        return ans;
     }
 };
