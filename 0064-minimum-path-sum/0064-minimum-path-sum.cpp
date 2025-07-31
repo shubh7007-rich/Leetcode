86161eace7b4173771ed03e59c0618818408d76c
Yogesh Bora <borashubham02@gmail.com>
2025-07-31 20:56:00 +0530

    Time: 4 ms (16.55%), Space: 15.5 MB (84.17%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,22 +1,23 @@
-#define ll long long
+// #define ll long long
 class Solution {
 public:
-    vector<vector<ll>> directions = {{1,0} , {0,1}};
+    vector<vector<int>> directions = {{1,0} , {0,1}};
     int dp[200][200];
+    int inf = 1e9;
 
-    ll func(ll r , ll c ,ll n , ll m , vector<vector<int>>& grid){
+    int func(int r , int c ,int n , int m , vector<vector<int>>& grid){
         if(r == n-1 && c == m-1){
             return grid[r][c];
         }
-        ll cost = INT_MAX;
+        int cost = inf;
 
         if(dp[r][c] != -1) return dp[r][c];
 
-        for(vector<ll>& vec : directions){
-            ll r1 = r + vec[0] , c1 = c + vec[1];
+        for(vector<int>& vec : directions){
+            int r1 = r + vec[0] , c1 = c + vec[1];
 
             if(r1 >= 0 && r1 < n && c1 >= 0 && c1 < m && grid[r1][c1] >= 0){
-                ll temp = grid[r][c];
+                int temp = grid[r][c];
                 grid[r][c] = -1;
                 cost = min(cost , temp + func(r1 , c1 , n , m  , grid));
                 grid[r][c] = temp;
@@ -27,7 +28,7 @@ public:
     }
     int minPathSum(vector<vector<int>>& grid) {
         memset(dp , -1 , sizeof(dp));
-        ll n = grid.size() , m = grid[0].size();
+        int n = grid.size() , m = grid[0].size();
         return func(0 ,0 , n , m , grid);
     }
 };
