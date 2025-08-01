7915583f48431b3cefb3232094e43a36c825c0ae
Yogesh Bora <borashubham02@gmail.com>
2025-08-01 09:17:06 +0530

    Time: 2 ms (30.91%), Space: 13.2 MB (75.2%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int t[3] = {1 , 7 , 30};
 
-    int dp[395][395];
+     int dp[395][395];
 
     int func(int ind , int day , vector<int>& days, vector<int>& costs){
         if(ind >= days.size()){
@@ -12,20 +12,23 @@ public:
 
         if(dp[ind][day] != -1) return dp[ind][day];
 
+        if(day >= days[ind]) cost = func(ind + 1 , day , days , costs);
+
+        miniCost = min(miniCost , cost);
+
         for(int i = 0 ; i < 3 ; i++){
-            int temp = days[ind] + t[i] - 1;
-            int j = ind + 1;
-            while(j < days.size() && temp >= days[j]){
-                j++;
-            }
-            cost = costs[i] + func(j , temp , days , costs);
+            cost = costs[i] + func(ind + 1 , days[ind] + t[i] - 1 , days , costs);
+
             miniCost = min(miniCost , cost);
         }
+
         return dp[ind][day] = miniCost;
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int day = 0 , ind = 0;
+
         memset(dp , -1 , sizeof(dp));
+
         return func(ind , day , days , costs);
     }
 };
