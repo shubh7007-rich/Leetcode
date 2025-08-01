fea428c7fcf3155b893a3c8f17862a32fdaa3108
Yogesh Bora <borashubham02@gmail.com>
2025-08-01 07:39:53 +0530

    Time: 3 ms (50.61%), Space: 18 MB (33.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,93 @@
 class Solution {
 public:
+    int partition(int l , int r , vector<int>& unique , unordered_map<int , int> & freq){
+        int pivotElem = unique[l];
+        int pivotFreq = freq[pivotElem];
+
+        int i = l+1 , j = r;
+
+        while(i <= j){
+            if(freq[unique[i]] < freq[unique[l]] && freq[unique[j]] >= freq[unique[l]]){
+                swap(unique[i] , unique[j]);
+                j--;
+                i++;
+            }
+
+            if(freq[unique[i]] >= freq[unique[l]]){
+                i++;
+            }
+
+            if(freq[unique[j]] < freq[unique[l]]){
+                j--;
+            }
+        } 
+
+        swap(unique[j] , unique[l]);
+
+        return j;
+    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int , int> mp;
+        // Approach 1 -- O(nlogn)
+        // unordered_map<int , int> freq;
 
-        for(int & i : nums) mp[i]++;
+        // for(int & i : nums) freq[i]++;  // O(n)
 
-        priority_queue<pair<int , int>> pq;
+        // priority_queue<pair<int , int>> pq;
 
-        for(auto [num , freq] : mp){
-            pq.push({freq , num});
+        // for(auto [num , freq] : freq){    // O(n)
+        //     pq.push({freq , num});      
+        // }
+
+        // // O(nlogn)
+
+        // vector<int> ans;
+
+        // while(k--){
+        //     if(!pq.empty()){
+        //         ans.push_back(pq.top().second);
+        //         pq.pop();
+        //     } 
+        // }
+
+        // return ans;
+
+        // Approach 2 --> QuickSelect
+
+        int n = nums.size();
+
+        unordered_map<int , int> freq;
+
+        for(int & i : nums) freq[i]++;
+
+        vector<int> unique , ans;
+
+        for(auto p : freq){
+            unique.push_back(p.first);
         }
 
-        vector<int> ans;
+        int l = 0 , r = unique.size()-1;
+    
+        int pivot_Idx = 0;
+        while(l <= r){
 
-        while(k--){
-            if(!pq.empty()){
-                ans.push_back(pq.top().second);
-                pq.pop();
-            } 
+            pivot_Idx = partition(l , r , unique , freq);
+
+            if(pivot_Idx == k-1){
+                break;
+            }
+
+            if(pivot_Idx > k-1){
+                r = pivot_Idx - 1;
+            }
+
+            if(pivot_Idx < k-1){
+                l = pivot_Idx + 1;
+            }
+        }
+
+
+        for(int i = 0 ; i <= pivot_Idx ; i++){
+            ans.push_back(unique[i]);
         }
 
         return ans;
