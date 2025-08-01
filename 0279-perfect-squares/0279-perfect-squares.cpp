b467343fba2ec402a8e3f3d02cdf1e87391881a8
Yogesh Bora <borashubham02@gmail.com>
2025-08-01 08:10:00 +0530

    Time: 67 ms (51.36%), Space: 8.9 MB (94.04%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int inf = 1e7;
-    int dp[100][10000];
-    int func(int ind , int sum , vector<int>& psq , int n){
+    int inf = 1e5;
+    int dp[10001];
+    int solve(int sum , int n){
+        if(sum > n) return inf;
+
         if(sum == n) return 0;
+        if(dp[sum] != -1) return dp[sum];
 
-        if(sum > n || ind >= psq.size()) return inf;
-        
-        if(dp[ind][sum] != -1) return dp[ind][sum];
+        int count = 0 , minCount = INT_MAX;
+        for(int i = 1; i*i <= (n-sum) ; i++){
+            int count = 1 + solve(sum + (i*i) , n);
 
-        int take = 1 + func(ind , sum + psq[ind] , psq , n);
-        int notTake = func(ind+1, sum , psq , n);
-
-        return dp[ind][sum] = min(take , notTake);
+            minCount = min(minCount , count);
+        }
+        return dp[sum] = minCount;
     }
     int numSquares(int n) {
-        vector<int> psq;
-
+        int sum = 0;
         memset(dp , -1 , sizeof(dp));
-
-        for(int i = 1 ; i*i <= n ; i++){
-            psq.push_back(i*i);
-        }
-        
-        return func(0 , 0 , psq , n);
+        return solve(sum , n);
     }
 };
