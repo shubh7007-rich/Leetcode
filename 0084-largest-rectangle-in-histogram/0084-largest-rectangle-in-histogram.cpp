687ea60e66c925b24c3b0ae3fefa85c48caa28b8
Yogesh Bora <borashubham02@gmail.com>
2025-08-02 17:56:19 +0530

    Time: 21 ms (64.55%), Space: 81.4 MB (64.25%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        /*
-            as we need to do it in O(n) , and we require the left boundary(i.e index + 1 of the histrogram smaller than the current histogram) and the right boundary index for
-        */
-        int n = heights.size();
+        // Approach 2 --> Doing it in one pass , as i don't need to traverse twice to get the leftBoundary and rightBoundary 
 
-        vector<int> leftBoundary(n) , rightBoundary(n);
+        int maxA = 0 , n = heights.size();
 
-        stack<int> stL , stR;   // push index , as we need (index + 1) for left boundary and (index - 1) for right boundary
+        stack<int> st;
+ 
+        for(int i = 0 ; i <= n ; i++){
+            while(!st.empty() && ((i == n) || (heights[st.top()] > heights[i]))){
+                int rightBoundary = i;
+                int height = heights[st.top()];
+                st.pop();
 
-        for(int i = 0 ; i < n ; i++){
-            while(!stL.empty() && (heights[stL.top()] >= heights[i])){
-                stL.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = rightBoundary - st.top() - 1;
+
+                maxA = max(maxA , width * height);
             }
-
-            if(stL.empty()){
-                leftBoundary[i] = 0;
-            }else{
-                leftBoundary[i] = stL.top() + 1;
-            }
-
-            stL.push(i);
+            st.push(i);
         }
 
-        for(int i = n-1 ; i >= 0 ; i--){
-            while(!stR.empty() && (heights[stR.top()] >= heights[i])){
-                stR.pop();
-            }
-
-            if(stR.empty()){
-                rightBoundary[i] = n-1;
-            }else{
-                rightBoundary[i] = stR.top() - 1;
-            }
-
-            stR.push(i);
-        }
-
-
-        int maxi = 0;
-
-        for(int i = 0 ; i < n ; i++){
-            maxi = max(maxi , (rightBoundary[i] - leftBoundary[i] + 1)*heights[i]);
-        }
-
-        return maxi;
+        return maxA;
     }
 };
