02683d01081219155e6afca4feabb149bb6ad196
Yogesh Bora <borashubham02@gmail.com>
2025-08-02 12:25:37 +0530

    Time: 282 ms (9.49%), Space: 39.5 MB (53.67%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int inf = -1e7;
+    int dp[600][101][101];
+    int func(int ind ,int c0 , int c1 ,vector<string>& strs, int m, int n , unordered_map<int , pair<int , int>>& mp){
+        if(ind >= strs.size()){
+            return 0;
+        }
+
+        int zero = mp[ind].first , one = mp[ind].second;
+
+        if(dp[ind][c0][c1] != -1) return dp[ind][c0][c1];
+        
+        int take = inf;
+        if((c0 + zero <= m) && (c1 + one <= n)){
+            take = 1 + func(ind+1 , c0 + zero , c1 + one , strs , m , n , mp);
+        }
+
+        int notTake = func(ind+1 , c0 , c1 , strs , m , n , mp);
+
+        return dp[ind][c0][c1] = max(take , notTake);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        // preprocessing
+         unordered_map<int , pair<int , int>> mp;
+
+         memset(dp , -1 , sizeof(dp));
+
+        for(int i = 0 ; i < strs.size() ; i++){
+            int zero = 0 , one = 0;
+            for(char ch : strs[i]){
+                if(ch == '0'){
+                    zero++;
+                }else{
+                    one++;
+                }
+            }
+            mp[i] = {zero , one};
+        }
+
+        return func(0 ,0 , 0 ,strs , m , n , mp);
+    }
+};
