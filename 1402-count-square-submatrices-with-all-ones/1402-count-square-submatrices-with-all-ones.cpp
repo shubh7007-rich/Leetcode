49cd12ba3d0f8a81fb4d6e993266fa454d030a95
Yogesh Bora <borashubham02@gmail.com>
2025-08-04 07:58:17 +0530

    Time: 19 ms (9.58%), Space: 28.9 MB (85.18%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
+    int dp[300][300];
+    int func(int i , int j , vector<vector<int>>& matrix, int n , int m){
+        if(i >= n || j >= m) return 0;
+
+        if(matrix[i][j] == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j]; 
+
+        int right = func(i , j+1 , matrix , n , m);
+        int diagonal = func(i+1 , j+1 , matrix , n , m);
+        int down = func(i+1 , j , matrix , n , m);
+
+        return dp[i][j] = 1 + min({right , diagonal , down});
+    }
     int countSquares(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size(), cnt = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                // Only start checking if the cell is 1
-                if(matrix[i][j] == 1) {
-                    // Try all possible square sizes starting from (i, j)
-                    for(int k = 1; i + k <= n && j + k <= m; k++) {
-                        bool isSquare = true;
-
-                        // Check all cells in the k x k square
-                        for(int x = i; x < i + k; x++) {
-                            for(int y = j; y < j + k; y++) {
-                                if(matrix[x][y] == 0) {
-                                    isSquare = false;
-                                    break;
-                                }
-                            }
-                            if(!isSquare) break;
-                        }
-
-                        if(isSquare) cnt++;
-                        else break; // If one square fails, no need to try larger one
-                    }
+        int cnt = 0 , n = matrix.size() , m = matrix[0].size();
+        memset(dp , -1 , sizeof(dp));
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == 1){
+                    cnt += func(i , j , matrix , n , m);
                 }
             }
         }
