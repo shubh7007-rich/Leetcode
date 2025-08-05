05a0191aca77e3fd6e50b61f8b65824858a7cfa4
Yogesh Bora <borashubham02@gmail.com>
2025-08-05 17:44:20 +0530

    Time: 4 ms (83.74%), Space: 8.8 MB (81.9%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    // int dp[25][25][101];
-    // map<tuple<int , int , int> , double> dp;
-    unordered_map<string , double> dp;
-    vector<vector<int>> directions = {{-2,-1} , {-2,1} , {-1,-2} , {1,-2} , {2,-1} , {2,1} , {-1,2} , {1,2}};
-    double func(int r , int c , int n , int k){
-        if(r < 0 || r >= n || c < 0 || c >= n) return 0;
+    double dp[25][25][101];
 
+    vector<vector<int>> directions = {{-2,-1} , {-2,1} , {-1,-2} , {1,-2} , {2,-1} , {2,1} , {-1,2} , {1,2}};
+    
+    double func(int i , int j , int n , int k){
+        if(i < 0 || i >= n || j < 0 || j >= n) return 0;
         if(k == 0) return 1;
 
+        if(dp[i][j][k] >= 0.0) return dp[i][j][k];
+
         double prob = 0;
-
-        // tuple<int , int , int> key = {r , c , k};
-        string key = to_string(r) + "," + to_string(c) + "," + to_string(k) ;
-
-        // if(dp[r][c][k] != -1) return dp[r][c][k];
-        if(dp.count(key)) return dp[key];
-
-        for(vector<int>& dir : directions){
-            prob += func(r + dir[0] , c + dir[1] , n , k-1);
+        for(auto& d : directions){
+            int ni = i + d[0];
+            int nj = j + d[1];
+            prob += func(ni, nj, n, k-1);
         }
 
-
-        return dp[key] = prob/8;
+        return dp[i][j][k] = prob / 8.0;
     }
+
     double knightProbability(int n, int k, int row, int column) {
-        // memset(dp , -1 , sizeof(dp));
-        
+        memset(dp , -1 , sizeof(dp));
         return func(row , column , n , k);
     }
 };
