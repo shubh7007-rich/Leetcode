b069ba09b4fd50a01fa3d05ffcc79d8a1905fe4a
Yogesh Bora <borashubham02@gmail.com>
2025-08-05 10:41:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (73.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int dp[45];
+    int dp[46];
+
     int func(int i , int n){
         if(i == n) return 1;
 
-        if(i > n ) return 0;
+        if(i > n) return 0;
 
-        int s1 = 0 , s2;
-        s1 = func(i + 1 , n);
-        s2 = func(i+2 , n);
+        if(dp[i] != -1) return dp[i];
 
-        return s1 + s2;
+        int take1 =  func(i+1 , n);
+        int take2 =  func(i+2 , n);
 
+        return dp[i] = take1 + take2;
     }
     int climbStairs(int n) {
+        memset(dp , -1 , sizeof(dp));
         return func(0 , n);
     }
 };
