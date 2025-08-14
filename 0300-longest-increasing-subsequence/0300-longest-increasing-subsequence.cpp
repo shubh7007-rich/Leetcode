2af1925c2c6365e60a78e9bcb334406dbda56edb
Yogesh Bora <borashubham02@gmail.com>
2025-08-14 15:58:18 +0530

    Time: 59 ms (67.1%), Space: 14.2 MB (73.67%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int dp[2501][2500];
-    int func(int prev , int ind , vector<int>& nums , int n){
-        if(ind >= n) return 0;
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size() , maxi = 1;
+        vector<int> dp(n , 1);
 
-        int take = 0 , skip = 0;
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i] , 1 + dp[j]);
+                }
+            }
 
-        if(dp[prev + 1][ind] != -1) return dp[prev + 1][ind];
-
-        if(prev == -1 || (nums[prev] < nums[ind])){
-            take = 1 + func(ind , ind + 1 , nums ,  n);
+            maxi = max(maxi , dp[i]);
         }
 
-        skip = func(prev , ind + 1 , nums ,  n);
-
-        return dp[prev + 1][ind] = max(skip , take);
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        memset(dp , -1 , sizeof(dp));
-        // int  maxi = 0;
-
-        int maxi = func(-1 , 0 , nums , nums.size());
-        
-
         return maxi;
-
     }
 };
