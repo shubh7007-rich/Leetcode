505365eaa4113d413ef2b506bfebffb0ee43d5db
Yogesh Bora <borashubham02@gmail.com>
2025-08-20 09:33:00 +0530

    Time: 22 ms (8.77%), Space: 29.2 MB (84.88%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int dp[300][300];
-    int func(int i , int j , vector<vector<int>>& matrix, int n , int m){
-        if(i >= n || j >= m) return 0;
+    int func(int i , int j , vector<vector<int>>& matrix){
 
+        if(i >= matrix.size() || j >= matrix[0].size()) return 0;
         if(matrix[i][j] == 0) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j]; 
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int right = func(i , j+1 , matrix , n , m);
-        int diagonal = func(i+1 , j+1 , matrix , n , m);
-        int down = func(i+1 , j , matrix , n , m);
+        int right = func(i , j+1 , matrix);
+        int dig = func(i+1 , j+1 , matrix);
+        int down = func(i+1 , j , matrix);
 
-        return dp[i][j] = 1 + min({right , diagonal , down});
+
+        return dp[i][j] = 1 + min({right , dig , down});
     }
+
     int countSquares(vector<vector<int>>& matrix) {
         int cnt = 0 , n = matrix.size() , m = matrix[0].size();
+
         memset(dp , -1 , sizeof(dp));
+
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(matrix[i][j] == 1){
-                    cnt += func(i , j , matrix , n , m);
-                }
+            for(int j = 0 ;  j < m ; j++){
+                cnt += func(i , j , matrix);
             }
         }
-
         return cnt;
     }
 };
