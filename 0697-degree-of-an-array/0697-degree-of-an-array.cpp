6b79fb071ff0c1879680e4f24cb3e32a8f48d076
Yogesh Bora <borashubham02@gmail.com>
2025-09-02 12:51:22 +0530

    Time: 10 ms (64.85%), Space: 30.2 MB (36.22%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -1,33 +1,60 @@
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        int deg = 0 , maxi = 0;
+        // Approach 1 - just expand window until we find the maximum frequency equals to deg and shrink when maximum freq in that window >= deg
 
-        unordered_map<int , int> freq1 , freq2;
+        // int deg = 0 , maxi = 0;
 
-        for(int i : nums){
-            freq1[i]++;
-            deg = max(deg , freq1[i]);
+        // unordered_map<int , int> freq1 , freq2;
+
+        // for(int i : nums){
+        //     freq1[i]++;
+        //     deg = max(deg , freq1[i]);
+        // }
+
+        // int i = 0 , mini = INT_MAX;
+
+        // for(int j = 0 ; j < nums.size() ; j++){
+        //     freq2[nums[j]]++;
+
+        //     maxi = max(maxi , freq2[nums[j]]);
+
+        //     while(maxi >= deg){
+        //         mini = min(mini , j-i+1);
+
+        //         freq2[nums[i]]--;
+
+        //         maxi = INT_MIN;
+
+        //         for(auto [num , f] : freq2){
+        //             maxi = max(maxi , f);
+        //         }
+        //         i++;
+        //     }
+        // }
+
+        // return mini;
+
+        // Approach 2 - optimal --> we need to find minimum length of the window with maximum freq == deg , so instead of expanding and shrinking the window , just calculate first and last position of all the element , and the element with frequency == deg , just calculate the length of that window
+
+        unordered_map<int , int> freq , firstPos , lastPos;
+        int n = nums.size() , deg = 0;
+        for(int i = 0 ; i < n ; i++){
+            freq[nums[i]]++;
+            deg = max(deg , freq[nums[i]]);
+
+            if(!firstPos.count(nums[i])){
+                firstPos[nums[i]] = i;
+            }
+
+            lastPos[nums[i]] = i;
         }
 
-        int i = 0 , mini = INT_MAX;
+        int mini = INT_MAX;
 
-        for(int j = 0 ; j < nums.size() ; j++){
-            freq2[nums[j]]++;
-
-            maxi = max(maxi , freq2[nums[j]]);
-
-            while(maxi >= deg){
-                mini = min(mini , j-i+1);
-
-                freq2[nums[i]]--;
-
-                maxi = INT_MIN;
-
-                for(auto [num , f] : freq2){
-                    maxi = max(maxi , f);
-                }
-                i++;
+        for(auto [num , f] : freq){
+            if(f == deg){
+                mini = min(mini , lastPos[num] - firstPos[num] + 1);
             }
         }
 
