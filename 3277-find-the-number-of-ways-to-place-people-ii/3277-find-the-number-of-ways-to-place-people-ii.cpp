60e7a284af654aeb8cd8e7ca9677aba0034976d1
Yogesh Bora <borashubham02@gmail.com>
2025-09-03 12:49:27 +0530

    Time: 167 ms (20.21%), Space: 41.2 MB (35.11%) - LeetHub

--- 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
@@ -15,8 +15,8 @@ public:
         // }
 
         for (int i = 0; i < points.size(); i++) {
+            int y = INT_MIN;
             for (int j = i + 1; j < points.size(); j++) {
-
                 int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0],
                     y2 = points[j][1];
                 bool flag = true;
@@ -24,18 +24,12 @@ public:
                 // if(x1 < x2 && y2 > y1) continue;
 
                 if (x1 <= x2 && y1 >= y2) {
-                    for (int k = i; k < j; k++) {
-                        if (k != i && k != j) {
-                            int x3 = points[k][0], y3 = points[k][1];
-                            if (x3 >= x1 && x3 <= x2 && y3 >= min(y1, y2) &&
-                                y3 <= max(y2, y1)) {
-                                flag = false;
-                                break;
-                            }
-                        }
-                    }
-                    if (flag)
+                    
+                    if(y2 > y){
                         cnt++;
+
+                        y = max(y , y2);
+                    }
                 }
             }
         }
