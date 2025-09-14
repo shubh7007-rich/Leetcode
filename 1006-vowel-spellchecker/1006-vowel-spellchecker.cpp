da07fc9f4042fdc1ea45850e5115b644d5d6bb09
Yogesh Bora <borashubham02@gmail.com>
2025-09-14 22:57:50 +0530

    Time: 2599 ms (5.06%), Space: 37 MB (97.47%) - LeetHub

---
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+    vector<string> spellchecker(vector<string>& wordlist,
+                                vector<string>& queries) {
+        unordered_set<string> exact;
+        unordered_map<string, int> mp;
+        int ind = 0;
+        for (auto& wr : wordlist) {
+            exact.insert(wr);
+
+            string temp2 = wr;
+
+            for (char& ch : temp2) {
+                ch = tolower(ch);
+            }
+
+            if(!mp.count(temp2)){
+                mp[temp2] = ind;
+            }
+            ind++;
+        }
+
+        vector<string> ans;
+
+        for (string& str : queries) {
+            if (exact.count(str)) {
+                ans.push_back(str);
+            } else {
+                bool flag = false;
+
+                string temp = str;
+
+                for (char& ch : temp) {
+                    ch = tolower(ch);
+                }
+
+                // for (string& s : wordlist) {
+                //     string temp2 = s;
+
+                //     for (char& ch : temp2) {
+                //         ch = tolower(ch);
+                //     }
+
+                //     if (temp == temp2) {
+                //         ans.push_back(s);
+                //         flag = true;
+                //         break;
+                //     }
+                // }
+
+                if(mp.count(temp)){
+                    ans.push_back(wordlist[mp[temp]]);
+                    continue;
+                }
+
+                bool flag2 = false;
+               
+
+                    for (string& s : wordlist) {
+                        string temp2 = s;
+
+                        if (temp.length() != temp2.length())
+                            continue;
+
+                        for (char& ch : temp2) {
+                            ch = tolower(ch);
+                        }
+                        bool mila = true;
+                        for (int i = 0; i < temp.length(); i++) {
+                            if (temp2[i] != temp[i]) {
+                                if (!isVowel(temp2[i]) || !isVowel(temp[i])) {
+                                    mila = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (mila) {
+                            ans.push_back(s);
+                            flag2 = true;
+                            break;
+                        }
+                    }
+                
+
+                if (!flag2) {
+                    ans.push_back("");
+                }
+            }
+        }
+
+        return ans;
+    }
+};
