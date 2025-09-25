de5ef9013e19ab19418eb091d9474843aae4c4c1
Yogesh Bora <borashubham02@gmail.com>
2025-09-25 18:40:56 +0530

    Time: 15 ms (59.97%), Space: 22.1 MB (40.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,51 @@
 class Solution {
 public:
-    
+    bool isPalindrome(string & str){
+        int l = 0 , r = str.length()-1;
+        while(l <= r){
+            if(str[l] != str[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
     string longestPalindrome(string s) {
-        int n = s.length(), maxi = 0;
+        int n = s.length() , maxi = 0;
         string ans = "";
-        for (int i = 0; i < n; i++) {
-           
-           // odd 
-           int l = i , r = i;
+        for(int i = 0 ; i < n; i++){
+            int l = i , r = i;
 
-           while(l >= 0 && r < n){
-                if(s[l] != s[r]) break;
+            while(l >= 0 && r < n){
+                if(s[l] == s[r]){
+                    if((r-l+1) > maxi){
+                        maxi = r-l+1;
+                        ans = s.substr(l , r-l+1);
+                    }
+                }else{
+                    break;
+                }
+                l--;r++;
+            }
+
+            // even
+
+            l = i , r = i+1;
+
+            while(l >= 0 && r < n){
+                if(s[l] == s[r]){
+                    if(maxi < (r-l+1)){
+                        maxi = r-l+1;
+                        ans = s.substr(l , r-l+1);
+                    }
+                }else{
+                    break;
+                }
                 l--;
                 r++;
-           }
-           l++;r--;
-           if(r-l+1 > maxi){
-                ans = s.substr(l , r-l+1);
-                maxi = r-l+1;
-           }
-
-           // even 
-           l = i , r = i+1;
-
-           while(l >= 0 && r < n){
-                if(s[l] != s[r]) break;
-                l--;
-                r++;
-           }
-           l++;r--;
-           if(r-l+1 > maxi){
-                ans = s.substr(l , r-l+1);
-                maxi = r-l+1;
-           }
+            }
         }
 
         return ans;
+
     }
 };
