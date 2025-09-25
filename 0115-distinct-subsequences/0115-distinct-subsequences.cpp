96daf51a3c7cb08ca6c9fb980294f6db1f5480d0
Yogesh Bora <borashubham02@gmail.com>
2025-09-25 22:31:24 +0530

    Time: 31 ms (54.84%), Space: 12.9 MB (81.27%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int dp[1000][1000];
+    int func(string & s , string & t , int i , int j){
+        if(j == t.size()) return 1;
 
-    int countWays(string& s, string& t, int i, int j) {
-        if (j == t.size())
-            return 1; // matched entire t
-        if (i == s.size())
-            return 0; // no s left, can't match
+        if(i == s.size()) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if (s[i] == t[j]) {
-            return dp[i][j] = countWays(s, t, i + 1, j + 1) + countWays(s, t, i + 1, j);
-        } else {
-            return dp[i][j] = countWays(s, t, i + 1, j);
+        if(s[i] == t[j]){
+            return dp[i][j] = func(s , t , i+1 , j+1) + func(s , t , i+1 , j);
+        }else{
+            return dp[i][j] = func(s , t , i+1 , j);
         }
     }
-
-    int numDistinct(string s, string t) { 
+    int numDistinct(string s, string t) {
         memset(dp , -1 , sizeof(dp));
 
-        return countWays(s, t, 0, 0); }
+        return func(s , t , 0 , 0);
+    }
 };
