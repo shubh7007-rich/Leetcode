3d77437c4864ebcc074f9b2aecec6fd751de7f5c
Yogesh Bora <borashubham02@gmail.com>
2025-09-26 22:14:04 +0530

    Time: 51 ms (9.17%), Space: 51.4 MB (96.88%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -3,40 +3,36 @@ class Solution {
 public:
     int mod = 1e9 + 7;
 
-    // int power(int a , int b){
-    //     ll result = 1;
-    //     while(b){
-    //         if(b&1){
-    //             result = (result * a) % mod;
-    //         }
-    //         a = (a * 1ll * a) % mod;
-    //         b >>= 1;
-    //     }
+    ll power(ll a , ll b){
+        ll res = 1;
 
-    //     return result % mod;
-    // }
+        while(b){
+            if(b&1){
+                res = (res * a) % mod;
+            }
+            a = (a * a) % mod;
+            b = b >> 1;
+        }
+
+        return res;
+
+    }
     int numSubseq(vector<int>& nums, int target) {
-        int cnt = 0;
-        int n = nums.size();
-
-        vector<ll> power(n , 1);
-        ll a = 2;
-        for(int i = 1; i < n ; i++){
-            power[i] = a;
-            a = (a*2)%mod;
-        }
-
         sort(nums.begin() , nums.end());
-
+        int cnt = 0;
         for(int i = 0 ; i < nums.size() ; i++){
-            int tar = target - nums[i];
+            int num = nums[i];
 
-            int idx = upper_bound(nums.begin() , nums.end() , tar) - nums.begin() - 1;
+            if(num > target) break;
 
-            if( idx >= i ) cnt = (cnt + power[idx-i])%mod;
+            int toFind = target - num;
+
+            int idx = upper_bound(nums.begin() , nums.end() , toFind) - nums.begin() - 1;
+
+            if(idx >= i){
+                cnt = (cnt + (power(2 , idx - i))) % mod;
+            }
         }
-
-        // for(ll & i : power) cout << i << " ";
 
         return cnt;
     }
