38ba988548fd7d01d73ddb8f3895debcb5e836f8
Yogesh Bora <borashubham02@gmail.com>
2025-09-29 09:57:34 +0530

    Time: 4 ms (25.12%), Space: 14.1 MB (36%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    void func(int ind , int sum , vector<int>& temp , vector<int>& candidates, int target , set<vector<int>> & ans){
-        if(ind >= candidates.size()){
+    void func(int ind ,int sum ,vector<int> & temp , vector<vector<int>>& ans , vector<int>& nums , int target){
+        if(ind >= nums.size()){
             if(sum == target){
-                ans.insert(temp);
+                ans.push_back(temp);
             }
             return;
         }
+
         if(sum > target) return;
 
-        sum += candidates[ind];
-        temp.push_back(candidates[ind]);
-        func(ind + 1 , sum , temp , candidates , target , ans);
-        sum -= candidates[ind];
-        temp.pop_back();
-        while(ind + 1 < candidates.size() && (candidates[ind] == candidates[ind+1])){
-            ind++;
-        }
-        func(ind+1 , sum , temp , candidates , target , ans);
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>> ans;
-        sort(candidates.begin() , candidates.end());
-        int sum = 0;
+            temp.push_back(nums[ind]);
+            sum += nums[ind];
+
+            func(ind + 1 , sum , temp , ans , nums , target);
+            sum -= nums[ind];
+            temp.pop_back();
+            while(ind + 1 < nums.size() && nums[ind] == nums[ind + 1]){
+                ind++;
+            }
         
+        func(ind + 1, sum , temp , ans , nums , target);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candi, int target) {
+        vector<vector<int>> ans;
         vector<int> temp;
-
-        func(0 , sum , temp , candidates , target , ans);
-
-        vector<vector<int>> yo(ans.begin() , ans.end());
-
-        return yo;
+        sort(candi.begin() ,candi.end());
+        func(0 ,0 , temp , ans , candi , target);
+        return ans;
     }
 };
