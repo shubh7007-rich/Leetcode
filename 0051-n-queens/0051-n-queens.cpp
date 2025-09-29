b31b88a9e592a2181d14770d99fb74b698d8c233
Yogesh Bora <borashubham02@gmail.com>
2025-09-29 17:50:07 +0530

    Time: 4 ms (37.42%), Space: 10.1 MB (91.8%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     bool isSafe(int row , int col , vector<string>& board , int n){
-        int r1 = row - 1;
-        int c1 = col - 1;
+        int r = row , c = col;
 
-        while(r1 >= 0 && c1 >= 0){
-            if(board[r1][c1] == 'Q') return false;
-            r1--;
-            c1--;
-        }
-        c1 = col - 1 , r1 = row+1;
-        while(r1 < n && c1 >= 0){
-            if(board[r1][c1] == 'Q') return false;
-            r1++;
-            c1--;
+        while(c >= 0){
+            if(board[r][c] == 'Q') return false;
+            c--;
         }
 
-        r1 = row , c1 = col-1;
-        while(c1 >= 0){
-            if(board[r1][c1] == 'Q') return false;
-            c1--;
+        c = col;
+
+        while(r >= 0 && c >= 0){
+            if(board[r][c] == 'Q') return false;
+            c--;r--;
+        }
+        r = row;
+        c = col;
+        while(r < n && c >= 0){
+            if(board[r][c] == 'Q') return false;
+            c--;r++;
         }
 
         return true;
+
+
     }
-    void func(int col , vector<string>& board , int n , vector<vector<string>>& ans){
+    void func(int col , vector<string>& board , vector<vector<string>>& ans , int n){
         if(col >= n){
             ans.push_back(board);
             return;
@@ -33,27 +34,25 @@ public:
         for(int row = 0 ; row < n ; row++){
             if(isSafe(row , col , board , n)){
                 board[row][col] = 'Q';
-                func(col+1 , board , n , ans);
+                func(col+1 , board , ans , n);
                 board[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board;
         string str(n , '.');
-
+        vector<string> board;
         vector<vector<string>> ans;
-
         for(int i = 0 ; i < n ; i++){
             board.push_back(str);
         }
 
         for(int row = 0 ; row < n ; row++){
             board[row][0] = 'Q';
-            func(1 , board , n , ans);
+            func(1 , board , ans , n);
             board[row][0] = '.';
         }
 
-        return ans;
+        return ans; 
     }
 };
