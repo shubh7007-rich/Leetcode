bc2ea7f659beec28f90e3211458189b4eff570b5
Yogesh Bora <borashubham02@gmail.com>
2025-09-29 12:26:49 +0530

    Time: 30 ms (90.32%), Space: 53.3 MB (93.66%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    bool isPalindrome(int start , int end , string & s){
-
-        while(start < end){
-            if(s[start] != s[end]) return false;
-            start++;end--;
+    bool isPalindrome(int l , int r , string & s){
+        while(l <= r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
         }
-
         return true;
     }
-    void func(int ind , vector<string>& temp , vector<vector<string>>& ans , string & s , int n){
-        if(ind >= n){
+
+    void func(int start , string& s , vector<string>& temp , vector<vector<string>>& ans){
+        if(start >= s.length()){
             ans.push_back(temp);
             return;
         }
 
-        for(int i = ind ; i < n ; i++){
-            if(isPalindrome(ind , i , s)){
-                temp.push_back(s.substr(ind , i-ind+1));
-                func(i+1 , temp , ans , s , n);
+        for(int i = start ; i < s.length() ; i++){
+            if(isPalindrome(start , i , s)){
+                temp.push_back(s.substr(start , i-start+1));
+                func(i+1 , s , temp , ans);
                 temp.pop_back();
             }
+            
         }
     }
     vector<vector<string>> partition(string s) {
-        int n = s.length();
-
-        vector<string> temp;
         vector<vector<string>> ans;
-
-        func(0 , temp , ans , s , n);
-
-        // for(int i = 0 ; i < n ; i++){
-        //     if(isPalindrome(0 , i , s)){
-        //         temp.push_back(s.substr(0 , i - 0 + 1));
-        //         func(i+1 , temp , ans , s , n);
-        //         temp.pop_back();
-        //     }
-        // }
+        vector<string> temp;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(isPalindrome(0 , i , s)){
+                temp.push_back(s.substr(0 , i+1));
+                func(i+1 , s , temp , ans);
+                temp.pop_back();
+            }
+        }
 
         return ans;
     }
