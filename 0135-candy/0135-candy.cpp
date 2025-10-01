cfdc726e1f4c39d201e7487e49b390ad76e400cf
Yogesh Bora <borashubham02@gmail.com>
2025-10-01 23:15:30 +0530

    Time: 3 ms (26.72%), Space: 22.8 MB (64.02%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    /* 
-        So here for every index i need satisfy the conditions
-        1. Each child must have at least one candy.
-        2. Children with a higher rating get more candies than their neighbors.
-        So for every index (expect the first and the last) we need to check it's left and  right neighbour to satisfying the condition
-        So for ques like these first go from L 2 R satisfying the condition left neighbour ko dekhte huwe and then go from R 2 L satisfying the condition right neighbour ko dekhte huwe
-    */
     int candy(vector<int>& ratings) {
+        /*
+            [3 , 2 , 1 , 2]  --> ratings
+            [3 , 2 , 1 , 2]
+
+
+            [1 , 3 , 4 , 5 , 2]
+            [1 , 2 , 3 , 4 , 1]
+        
+        */
         int n = ratings.size();
         vector<int> ans(n , 1);
 
-        if(n == 1) return 1;
-
         for(int i = 1 ; i < n ; i++){
             if(ratings[i] > ratings[i-1]){
-                ans[i] = ans[i-1] + 1;
+                ans[i] = max(ans[i] , ans[i-1] + 1);
             }
         }
 
         for(int i = n-2 ; i >= 0 ; i--){
-            if(ratings[i] > ratings[i+1] && ans[i] <= ans[i+1]){
-                ans[i] = ans[i+1] + 1;
+            if(ratings[i] > ratings[i+1]){
+                ans[i] = max(ans[i] , ans[i+1] + 1);
             }
         }
-        int sum = 0;
 
-        for(int & i : ans) sum += i;
-
-        return sum;
+        return accumulate(ans.begin() , ans.end() , 0);
     }
-};
+}; 
