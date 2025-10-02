ba165bf3d42e636702bf7ba565381c7c7aea8976
Yogesh Bora <borashubham02@gmail.com>
2025-10-02 18:31:30 +0530

    Time: 0 ms (100%), Space: 15 MB (39.8%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -6,26 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* node, int level, vector<int>& ans) {
-        if(node) {
-            if (level == ans.size())
-                ans.push_back(node->val);
-
-            solve(node->right, level + 1, ans);
-            solve(node->left, level + 1, ans);
-        }
-    }
     vector<int> rightSideView(TreeNode* root) {
+        if(!root) return {};
+        
+        queue<TreeNode*> que;
+        int lvl = 0 , l = -1;
+        que.push(root);
         vector<int> ans;
 
+        while(!que.empty()){
+            int size = que.size();
 
-            solve(root, 0, ans);
+            while(size--){
+                auto fro = que.front();
+                que.pop();
+
+                if(l < lvl){
+                    ans.push_back(fro->val);
+                    l++;
+                }
+
+                if(fro->right) que.push(fro->right);
+                if(fro->left) que.push(fro->left);
+            }
+
+            lvl++;
+        }
 
         return ans;
     }
