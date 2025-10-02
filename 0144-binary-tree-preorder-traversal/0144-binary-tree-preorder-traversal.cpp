46ae2eabc8af10dc8af174d834dd1faa9cd21ca2
Yogesh Bora <borashubham02@gmail.com>
2025-10-02 09:20:08 +0530

    Time: 0 ms (100%), Space: 11 MB (15.28%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,18 +11,43 @@
  */
 class Solution {
 public:
-    void func(TreeNode* root , vector<int>& ans){
-        if(root == NULL) return;
+    // void func(TreeNode* root , vector<int>& ans){
+    //     if(root == NULL) return;
 
-        ans.push_back(root->val);
-        func(root->left , ans);
-        func(root->right , ans);
-    }
+    //     ans.push_back(root->val);
+    //     func(root->left , ans);
+    //     func(root->right , ans);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
         // root left right
         vector<int> ans;
 
-        func(root , ans);
+        /*
+            Morris traversal --> Preorder , Inorder 
+
+            Preorder Traversal --> We will start from the root , and whenever i reach a node that is not null , i will push it into my stack (as well as in my ans array) and move towards the left and when i reach node that is null , i will pop the top of the stack and move towards the right of the node that has been popped
+
+        */
+
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                ans.push_back(node->val);
+                node = node->left;
+            }else{
+                if(st.empty()) break;
+
+                TreeNode* t = st.top();
+                st.pop();
+
+                node = t->right;
+            }
+
+        }
+
         return ans;
     }
 };
