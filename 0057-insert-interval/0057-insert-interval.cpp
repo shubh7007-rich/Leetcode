7859dc48f55994f23217c98af922cde89bee1b49
Yogesh Bora <borashubham02@gmail.com>
2025-10-02 08:47:27 +0530

    Time: 0 ms (100%), Space: 21.7 MB (46.31%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,25 +2,29 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& inv, vector<int>& newinv) {
         vector<vector<int>> ans;
-        inv.push_back(newinv);
-        sort(inv.begin() , inv.end());
 
-        int start = inv[0][0] , end = inv[0][1];
+        // if we already have been given non overlapping intervals so just traverse in the inv array and check if u can merge it with some intervals if not just push it and rest of the intervals;
 
-        int yo = newinv[0] , ya = newinv[1];
-
-        for(int i = 1 ; i < inv.size() ; i++){
-            if(end < inv[i][0]){
-                ans.push_back({start , end});
-                start = inv[i][0];
-                end = inv[i][1];
+        int i = 0;
+        while(i < inv.size()){
+            if(inv[i][1] < newinv[0]){
+                ans.push_back(inv[i]);
+            }else if(inv[i][0] > newinv[1]){
+                break;
             }else{
-                end = max(end , inv[i][1]);
+                newinv[0] = min(inv[i][0] , newinv[0]);
+                newinv[1] = max(inv[i][1] , newinv[1]);
             }
+
+            i++;
         }
 
-        ans.push_back({start , end});
+        ans.push_back(newinv);
 
+        while(i < inv.size()){
+            ans.push_back(inv[i]);
+            i++;
+        }
 
         return ans;
     }
