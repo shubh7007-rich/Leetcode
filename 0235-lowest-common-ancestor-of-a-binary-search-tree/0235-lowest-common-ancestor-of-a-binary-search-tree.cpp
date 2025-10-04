b411da9e31327caa36be6b803ee5feb0f5726403
Yogesh Bora <borashubham02@gmail.com>
2025-10-04 18:46:30 +0530

    Time: 21 ms (67.89%), Space: 23.6 MB (33.42%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool getPath(TreeNode* root , TreeNode* target , vector<TreeNode*>& path){
+        if(root == NULL) return false;
+
+        path.push_back(root);
+        
+        if(root == target) return true;
+
+        if(getPath(root->left , target , path)) return true;
+        if(getPath(root->right , target , path)) return true;
+        path.pop_back();
+
+        return false;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        /*
+            Brute Force approach --> just calculate the path from the root node to p and to q , and take the rightmost common node
+
+        */
+
+        // coding the brute force approach
+        vector<TreeNode*> path1 ,path2;
+
+        getPath(root , p , path1);
+        getPath(root , q , path2);
+
+        TreeNode* ans = NULL;
+
+        for(int i = 0 ; i < min(path1.size() , path2.size()) ; i++){
+            if(path1[i] == path2[i]){
+                ans = path1[i];
+            }
+        }
+
+        return ans;
+    }
+};
