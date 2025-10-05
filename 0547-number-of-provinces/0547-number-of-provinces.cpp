e1bd488dfdbb3b59899eb30e758faec0a29b35a4
Yogesh Bora <borashubham02@gmail.com>
2025-10-05 18:11:56 +0530

    Time: 0 ms (100%), Space: 19.6 MB (66.74%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int find(int node , vector<int>& parent){
+        if(node == parent[node]) return node;
+
+        return parent[node] = find(parent[node] , parent);
+    }
+
+    bool merge(int n1 , int n2 , vector<int>& parent , vector<int>& rank){
+        int p1 = find(n1 , parent) , p2 = find(n2 , parent);
+
+        if(p1 == p2) return false;
+
+        if(rank[p1] > rank[p2]){
+            parent[p2] = p1;
+        }else if(rank[p1] < parent[p2]){
+            parent[p1] = p2;
+        }else{
+            rank[p1]++;
+            parent[p2] = p1;
+        }
+
+        return true;
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        
+        vector<int> parent(n) , rank(n , 0);
+
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i;
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == j || !isConnected[i][j]) continue;
+
+                merge(i , j , parent , rank);
+            }
+        }
+        set<int> st;
+        for(int i = 0 ; i < n ; i++){
+            st.insert(find(i , parent));
+        }
+
+        return st.size();
+    }
+};
