199aa8f211405d097f1a996a1537e678683a5bdd
Yogesh Bora <borashubham02@gmail.com>
2025-10-10 09:53:12 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.99%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ME = nums[0] , cnt = 1;
-
-        // boyer-moore voting algo
+        int me = nums[0] , cnt = 1;
 
         for(int i = 1 ; i < nums.size() ; i++){
-            if(cnt == 0){
-                ME = nums[i]; cnt = 1;
-            }else if(nums[i] == ME){
+            if(nums[i] == me){
                 cnt++;
+            }else if(cnt == 0){
+                me = nums[i];
+                cnt = 1;
             }else{
                 cnt--;
             }
         }
 
-        // as here majority element always exists , so we don't need to verify
-
-        return ME;
+        return me;
     }
 };
