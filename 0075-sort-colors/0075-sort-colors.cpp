eabbb0ff7557af1e80804f30b345835da53c4ab3
Yogesh Bora <borashubham02@gmail.com>
2025-10-10 18:10:15 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,61 +1,42 @@
 class Solution {
 public:
-    // void merge(int lb , int mid , int ub , vector<int>& nums){
-    //     vector<int> temp;
-    //     int p1 = lb , p2 = mid + 1;
-
-    //     while(p1 <= mid && p2 <= ub){
-    //         if(nums[p1] <= nums[p2]){
-    //             temp.push_back(nums[p1]);
-    //             p1++;
-    //         }else{
-    //             temp.push_back(nums[p2]);
-    //             p2++;
-    //         }
-    //     }
-
-    //     while(p1 <= mid){
-    //         temp.push_back(nums[p1]);
-    //         p1++;
-    //     }
-    //     while(p2 <= ub){
-    //          temp.push_back(nums[p2]);
-    //          p2++;
-    //     }
-    //     int p3 = 0;
-
-    //     for(int i = lb ; i <= ub ; i++){
-    //         nums[i] = temp[i - lb];
-    //     }
-    // }
-    // void mergeS(int lb , int ub , vector<int>& nums){
-    //     if(lb < ub){
-    //         int mid = (lb + ub)/2;
-
-    //         mergeS(lb , mid , nums);
-    //         mergeS(mid+1 , ub , nums);
-    //         merge(lb , mid , ub , nums);
-    //     }
-    // }
     void sortColors(vector<int>& nums) {
-        int lb = 0 , ub = nums.size() - 1;
-        // mergeS(lb , ub , nums);
+        /*
+            [2 , 0 , 2 , 1, 1 , 0]
 
-        int low = 0 , mid = 0 , high = ub;
+            1st pass --> 
+            [0 , 2 , 2 , 1, 1, 0]
+            [0 , 2,  2, 1, ,1 , 0]
+            [0 , 2,  1, 2, ,1 , 0]
+            [0 , 2,  1, 1, ,2 , 0]
+            [0 , 2,  1, 1, ,0 , 2]
 
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[mid] , nums[low]);
-                low++;mid++;
-            }else if(nums[mid] == 1){
-                mid++;
-            }else{
-                swap(nums[high] , nums[mid]);
-                high--;
+            2nd pass  -->
+            [0 , 2,  1, 1, ,0 , 2]
+            [0 , 1,  2, 1, ,0 , 2]
+            [0 , 1,  1, 2, ,0 , 2]
+            [0 , 1,  1, 0, ,2 , 2]
+            [0 , 1,  1, 0, 2 , 2]
+            
+
+            
+        */
+
+        // bubble sort --> O(n^2)
+
+        for(int i = 0 ; i < nums.size()-1; i++){
+            bool swapped = false;
+            for(int j = 0 ; j < nums.size() - i - 1 ; j++){
+                if(nums[j] > nums[j+1]){
+                    swap(nums[j] , nums[j+1]);
+                    swapped = true;
+                }
             }
+            if(!swapped) break;
         }
 
+        // merge sort --> O(n logn)
 
-
+        // low mid high , low points to 0 , mid points to 1 , high points to 2
     }
 };
