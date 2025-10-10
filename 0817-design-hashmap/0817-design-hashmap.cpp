9cb317b13b83facc30773a90e26f81d6f56544c6
Yogesh Bora <borashubham02@gmail.com>
2025-10-10 15:55:20 +0530

    Time: 19 ms (77.35%), Space: 74.9 MB (41.24%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,23 +1,76 @@
 class MyHashMap {
 public:
 
-    // vector<list<pair<int , int>>> vec;
-    vector<int> vec;
+    int M;
+    vector<list<pair<int , int>>> vec;
+
+    int getInd(int key){
+        return key % M;
+    }
+
+    /*
+        hash(int key) return key % 10;
+        5 31
+        105 % 100 --> 5
+
+        open addressing  --> linear probing and quadratic probing  +(1)^2 +(2)^2 ......
+
+
+        separate chaining --> we will be using this
+        5 1  --> 35 3  --> 75 2 
+
+        0  1 2 3 4  5   6     7
+                   5,1 35,3 75,2
+    */
 
     MyHashMap() {
-        vec = vector<int>(1000001 , -1);
+        M = 15000;
+        vec = vector<list<pair<int , int>>>(M , list<pair<int , int>>{});
     }
     
     void put(int key, int value) {
-        vec[key] = value;
+        int idx = getInd(key);
+
+        auto & chain = vec[idx];
+        bool flag = false;
+        for(auto & p : chain){
+            if(p.first == key){
+                p.second = value;
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag){
+            chain.push_back({key , value});
+        }
     }
     
     int get(int key) {
-        return vec[key];
+        int idx = getInd(key);
+
+        auto & chain = vec[idx];
+
+        for(auto & p : chain){
+            if(p.first == key){
+                return p.second;
+            }
+        }
+
+        return -1;
     }
     
     void remove(int key) {
-        vec[key] = -1;
+        int idx = getInd(key);
+
+        auto & chain = vec[idx];
+
+        for(auto it = chain.begin() ; it != chain.end() ; ++it){
+            if(it->first == key){
+                chain.erase(it);
+                break;
+            }
+        }
     }
 };
 
