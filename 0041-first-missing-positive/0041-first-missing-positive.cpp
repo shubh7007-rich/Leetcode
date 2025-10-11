1a278f10011b3688a063d5e9b0551c72a3bbb70a
Yogesh Bora <borashubham02@gmail.com>
2025-10-11 08:09:12 +0530

    Time: 63 ms (14.74%), Space: 78.4 MB (8.43%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,22 +2,33 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         /*
-            brute force approach --> O(n log n)
+            brute force approach --> O(n logn)  
+
+            int p = 4
+
+            [3 , 0, 4 , -1 , 1]  --> [-1 ,0 , 1 , 2, 3]
 
             better approach --> O(n) and O(n)
 
+            n --> [1 , n+1]
+            n = 4
+            [1 , 2 , 1000, 4] --> 5
 
+            unordered_set<int> st(nums.begin() , nums.end());
 
-        
+            for(int i = 1 ; i <= n ; i++){
+                if()
+            }
+
+            optimal approach --> O(n) and O(1)
         */
 
         unordered_set<int> st(nums.begin() , nums.end());
 
-        for(int i = 1 ; i < 100001 ; i++){
+        for(int i = 1 ; i <= nums.size() ; i++){
             if(!st.count(i)) return i;
         }
 
-        return 100001;
-        
+        return nums.size() + 1; 
     }
 };
