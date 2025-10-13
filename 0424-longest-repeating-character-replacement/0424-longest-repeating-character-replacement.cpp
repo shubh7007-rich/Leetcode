0d94aaa92dd3fb969efec16fa169785e25304cc8
Yogesh Bora <borashubham02@gmail.com>
2025-10-13 07:37:05 +0530

    Time: 11 ms (27.99%), Space: 11 MB (18.86%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,29 +1,68 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // length of the window - MaxFreq of character <= k ---> valid window
+        /*
+            AABABBA  , k = 1
+
+            length of the window - most frequent character of the substring or window
+
+        */
+        int n = s.length() , ans = 0;
+
+        // for(int i = 0 ; i < n; i++){
+        //     vector<int> freq(26 , 0);
+        //     int mostFreq = 0;
+
+        //     for(int j = i ; j < n ; j++){
+
+        //         freq[s[j]-'A']++;
+
+        //         for(int e = 0 ; e < 26 ; e++){
+        //             if(freq[e] > mostFreq){
+        //                 mostFreq = freq[e];
+        //             }
+        //         }
+
+        //         if(j-i+1 - mostFreq > k) break;
+
+        //         ans = max(ans , j-i+1);
+        //     }
+        // }
+
+        // O(n*n*26) --> O(26 * n^2) --> O(n^2)
+
+        // return ans;
+
+        int i = 0;
 
         vector<int> freq(26 , 0);
-        int i = 0 , ans = 1;
-        for(int j  = 0 ; j < s.length() ; j++){
+
+        for(int j = 0 ; j < n ; j++){
             freq[s[j] - 'A']++;
-            int maxi = 0;
-            for(int & f : freq) maxi = max(maxi , f);
 
-            int lenOfWindow = j-i+1;
+            int mostFreq = 0;
 
-            while(lenOfWindow - maxi > k){
-                freq[s[i]-'A']--;
-                i++;
-
-                for(int & f : freq) maxi = max(maxi , f);
-
-                lenOfWindow = j-i+1;
+            for(int e : freq){
+                mostFreq = max(mostFreq , e);
             }
 
-            if(lenOfWindow - maxi <= k) ans = max(ans , j-i+1);
+            while(i < j && (j-i+1 - mostFreq > k)){
+                freq[s[i] - 'A']--;
 
+                for(int e : freq){
+                    mostFreq = max(mostFreq , e);
+                }
+                i++;
+            }
+
+            ans = max(ans , j-i+1);
         }
+
         return ans;
     }
 };
+
+/*
+    AABCACDA   k = 2
+    
+*/
