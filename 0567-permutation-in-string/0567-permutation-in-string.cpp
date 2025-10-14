f7b65ffad4da36c9dfa3a45feea71c1235d8aac6
Yogesh Bora <borashubham02@gmail.com>
2025-10-14 08:16:18 +0530

    Time: 0 ms (100%), Space: 9.8 MB (85.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,32 +6,42 @@ public:
 
             brute force --> to check all the substring and match the characters
 
+            better appraoch --> O(n^2)
+
+
+
             
 
         */
         int n = s2.length();
+
         vector<int> freqS1(26 , 0);
 
         for(char ch : s1) freqS1[ch - 'a']++;
 
-        for(int i = 0 ; i < n ; i++){
-            vector<int> freqSub(26 , 0);
+        int i = 0;
+        vector<int> freqSub(26 , 0);
 
-            for(int j = i ; j < n ; j++){
-                freqSub[s2[j] - 'a']++;
-                
-                bool flag = true;
-                for(int k = 0 ; k < 26 ; k++){
-                    if(freqS1[k] != freqSub[k]){
-                        flag = false;
-                        break;
-                    }
-                }
+        for(int j = 0 ; j < n ; j++){
+            freqSub[s2[j] - 'a']++;
 
-                if(flag) return true;
+            while(i < j && freqSub[s2[i] - 'a'] > freqS1[s2[i] - 'a']){
+                freqSub[s2[i] - 'a']--;
+
+                i++;
             }
-        }
+            bool flag = true;
+            for(int k = 0 ; k < 26 ; k++){
+                if(freqSub[k] != freqS1[k]){
+                    flag = false;
+                    break;
+                }
+            }
 
+            if(flag) return true;
+             
+        }
+        // O(26 * n)
         return false;
     }
 };
