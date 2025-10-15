817b2b5fd8b4721d092c89afd2e507f9c0572aca
Yogesh Bora <borashubham02@gmail.com>
2025-10-15 11:08:41 +0530

    Time: 28 ms (14.94%), Space: 23.6 MB (33.39%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,47 +10,29 @@
 
 class Solution {
 public:
-    bool getPath(TreeNode* root, vector<TreeNode*>& path ,  TreeNode* target){
-        if(root == NULL) return false;
-
-        path.push_back(root);
-
-        if(root->val == target->val) return true;
-
-        bool left = false , right = false;
-
-        if(target->val > root->val){
-            right = getPath(root->right , path , target);
-        }else{
-            left = getPath(root->left , path , target);
-        }
-
-        if(left || right) return true;
-
-        return false;
     
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         /*
             naive approach --> to get the path from root the nodes p and q and just compare and return the righmost common node in both the paths
 
+            better approach --> 
+
+            optimal approach --> if both nodes are smaller than the current node go to the left and both nodes are greater go the right , else return root (when either one is greater than the root and another one is smaller)
+
         */
 
-        vector<TreeNode*> path1 , path2;
-
-        getPath(root , path1 , p);
-        getPath(root , path2 , q);
-
+        if(root == NULL) return NULL;
+        
         TreeNode* ans = NULL;
 
-        for(int i = 0 ; i < min(path1.size() , path2.size()) ; i++){
-            if(path1[i] == path2[i]){
-                ans = path1[i];
-            }else{
-                break;
-            }
+        if(root->val > p->val && root->val > q->val){
+            ans = lowestCommonAncestor(root->left , p , q);
+        }else if(root->val < p->val && root->val < q->val){
+            ans = lowestCommonAncestor(root->right , p , q);
+        }else{
+            ans = root;
         }
 
-        return ans;
+        return ans;   
     }
 };
