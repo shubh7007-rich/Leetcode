f0444fe6eee3a2f071d27288baa88de5da2f63ac
Yogesh Bora <borashubham02@gmail.com>
2025-10-15 11:10:33 +0530

    Time: 24 ms (45.96%), Space: 23.6 MB (33.39%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -21,18 +21,30 @@ public:
 
         */
 
-        if(root == NULL) return NULL;
+        // if(root == NULL) return NULL;
         
-        TreeNode* ans = NULL;
+        // TreeNode* ans = NULL;
 
-        if(root->val > p->val && root->val > q->val){
-            ans = lowestCommonAncestor(root->left , p , q);
+        // if(root->val > p->val && root->val > q->val){
+        //     ans = lowestCommonAncestor(root->left , p , q);
+        // }else if(root->val < p->val && root->val < q->val){
+        //     ans = lowestCommonAncestor(root->right , p , q);
+        // }else{
+        //     ans = root;
+        // }
+
+        // return ans;   
+
+        while(root){
+            if(root->val > p->val && root->val > q->val){
+                root = root->left;
         }else if(root->val < p->val && root->val < q->val){
-            ans = lowestCommonAncestor(root->right , p , q);
+            root = root->right;
         }else{
-            ans = root;
+            return root;
+        }
         }
 
-        return ans;   
+        return NULL;
     }
 };
