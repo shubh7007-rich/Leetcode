49b118ac6147edf61c5f2f2e925189d78d54bb92
Yogesh Bora <borashubham02@gmail.com>
2025-10-16 07:10:30 +0530

    Time: 0 ms (100%), Space: 22.3 MB (91.31%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,23 +11,35 @@
  */
 class Solution {
 public:
-    map<pair<TreeNode* , int> , int> dp;
+    // // map<pair<TreeNode* , int> , int> dp;
 
-    int func(TreeNode* root , int f){
-        if(root == NULL) return 0;
+    // int func(TreeNode* root , int f){
+    //     if(root == NULL) return 0;
 
-        int take = 0 , notTake = 0;
+    //     int take = 0 , notTake = 0;
 
-        if(dp.count({root , f})) return dp[{root , f}];
+    //     if(dp.count({root , f})) return dp[{root , f}];
 
-        if(f){
-            take += root->val + func(root->left , 0) + func(root->right , 0);
-        }
-        notTake += func(root->left , 1) + func(root->right , 1);
+    //     if(f){
+    //         take += root->val + func(root->left , 0) + func(root->right , 0);
+    //     }
+    //     notTake += func(root->left , 1) + func(root->right , 1);
 
-        return dp[{root , f}] = max(take , notTake);
+    //     return dp[{root , f}] = max(take , notTake);
+    // }
+    pair<int , int> func(TreeNode* root){
+        if(root == NULL) return {0 , 0};
+
+        auto left = func(root->left);
+        auto right = func(root->right);
+
+        return {root->val + left.second + right.second , max(left.first , left.second) + max(right.first , right.second)};
     }
     int rob(TreeNode* root) {
-        return  func(root , 1);   
+        // return  func(root , 1);   
+
+        pair<int , int> ans = func(root);
+
+        return max(ans.first , ans.second);
     }
 };
