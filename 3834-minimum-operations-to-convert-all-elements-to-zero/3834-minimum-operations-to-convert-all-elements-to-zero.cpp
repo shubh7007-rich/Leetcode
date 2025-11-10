42e7b3702b5cc2aed93eb1e003f7d37c56fcc3c4
Yogesh Bora <borashubham02@gmail.com>
2025-11-10 17:38:13 +0530

    Time: 427 ms (20.79%), Space: 301.6 MB (28.09%) - LeetHub

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    /*
+        [5,6,7,4,6,7,6,7,5,5]
+
+    */
+    int minOperations(vector<int>& nums) {
+        stack<int> st;
+
+        int n = nums.size() , cnt = 0;
+
+        unordered_set<int> sett;
+
+        for(int i = 0 ; i < n ; i++){
+            if(st.empty()){
+                st.push(nums[i]);
+                sett.insert(nums[i]);
+                if(nums[i] != 0) cnt++;
+            }else{
+                if(st.top() > nums[i]){
+                    while(!st.empty() && st.top() > nums[i]){
+                        sett.erase(st.top());
+                        st.pop();
+                    }
+                    if(!sett.count(nums[i])) {
+                        
+                        st.push(nums[i]);
+                        if(nums[i] != 0) cnt++;
+                        sett.insert(nums[i]);
+                    }
+                }else{
+                    if(!sett.count(nums[i])){
+                        st.push(nums[i]);
+                        sett.insert(nums[i]);
+                        if(nums[i] != 0) cnt++;
+                    }
+                }
+                
+            }
+        }
+
+        return cnt;
+    }
+};
